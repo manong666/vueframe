c641e0025943a4e9a5a3d54c78d7599fb5debac5
wangsz <wangsz5@aisainfo.com>
2020-01-02 11:50:26 +0800

    fix 打包提示

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,23 @@
+const plugins = [
+  [
+    "import",
+    {
+      libraryName: "vant",
+      libraryDirectory: "es",
+      style: name => `${name}/style/less`
+      // style: true
+    },
+    "vant"
+  ],
+  "@babel/plugin-proposal-optional-chaining",
+  "@babel/plugin-proposal-nullish-coalescing-operator"
+];
+
+if (process.env.NODE_ENV === "production") {
+  plugins.push(["transform-remove-console", { exclude: ["error", "warn"] }]);
+}
+
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: [
-    [
-      "import",
-      {
-        libraryName: "vant",
-        libraryDirectory: "es",
-        style: name => `${name}/style/less`
-        // style: true
-      },
-      "vant"
-    ],
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator"
-  ]
+  plugins
 };

8f01a4acbebe0ae01676a958a94d5f5ad0188729
wangsz <wangsz5@aisainfo.com>
2020-01-06 12:03:17 +0800

    feat(pakage.json): 增加commit模板 yarn commit 提交命令

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,3 +1,4 @@
+const { exec } = require("child_process");
 const Client = require("scp2");
 const fs = require("fs");
 const path = require("path");
@@ -6,18 +7,32 @@ const privateKey = fs.readFileSync(
   path.join(process.cwd(), "scripts", "privateKey")
 );
 
-console.log(distPath);
-Client.scp(
-  distPath,
-  {
-    port: 22022,
-    host: "120.132.9.4",
-    path: "/app/fontend",
-    username: "root",
-    // privateKey
-    password: "xdjr0lxGu"
-  },
-  err => {
-    console.log(err);
+exec(`yarn build`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error:\n${error}`.red);
+    return;
   }
-);
+  // console.log(stderr);
+  // if (!!stderr) {
+  //   console.error(`exec stderr:\n${stderr}`.red);
+  //   return;
+  // }
+
+  console.log("stdout", stderr);
+  console.log("stdout", stdout);
+  console.log(`编译成功`);
+  Client.scp(
+    distPath,
+    {
+      port: 22,
+      host: "120.132.9.4",
+      path: "/app/fontend",
+      username: "root",
+      // privateKey
+      password: "xdjr0lxGu"
+    },
+    err => {
+      console.log("have error:", err);
+    }
+  );
+});
